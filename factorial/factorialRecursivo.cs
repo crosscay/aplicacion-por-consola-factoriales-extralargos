4b8cd084cba240aade0c10908d7ffe56678a94d5
crosscay <caicedocarlos0306@gmail.com>
2021-02-26 19:52:27 -0500

    agregando proyecto

---
+++ factorial/factorialRecursivo.cs
@@ -0,0 +1,35 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Numerics;
+
+class Solution
+{
+    // Complete the extraLongFactorials function below.
+    static BigInteger extraLongFactorials(int num)
+    {
+        if (num == 0 || num == 1)
+            return 1;
+        return num * extraLongFactorials(num - 1);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Digite el valor de n: ");
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("El factorial de: "+ n + " es: " + extraLongFactorials(n));
+        Console.ReadKey();
+    }
+}
---
+++ factorialConIterador/factorialConIterador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+
+namespace factorialConIterador
+{
+    class factorialConIterador
+    {
+        static void extraLongFactorials(int num)
+        {
+            BigInteger fact = new BigInteger();
+            fact = BigInteger.One;
+
+            for (int i = 1; i <= num; i++)
+            {
+                fact = BigInteger.Multiply(fact, i);
+            }
+            Console.Write("El factorial de: " + num + " es: " + fact);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Digite el valor de n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            extraLongFactorials(n);
+
+            Console.ReadKey();
+        }
+    }
+}
+
