020b52e0b2138e44ba15a1003afc7288195897a7
crosscay <caicedocarlos0306@gmail.com>
2021-02-26 20:01:14 -0500

    renombrando solucion

---
+++ factorialRecursivo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+class Solution
+{
+    static BigInteger extraLongFactorials(int num)
+    {
+        if (num == 0 || num == 1)
+            return 1;
+        return num * extraLongFactorials(num - 1);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Digite el valor de n: ");
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("El factorial de: " + n + " es: " + extraLongFactorials(n));
+        Console.ReadKey();
+    }
+}
